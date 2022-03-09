0ac622580f1783cb372f9c80aaa8e393ccd80f88
bharathnittech <bharath.nittech@gmail.com>
2022-03-09 21:10:29 +0530

    selenium Files

---
+++ src/selenium/basics/FirstSeleniumProgram.java
@@ -0,0 +1,12 @@
+package selenium.basics;
+
+import org.openqa.selenium.WebDriver;
+
+public class FirstSeleniumProgram {
+
+	public static void main(String[] args) {
+		WebDriver driver ;
+
+	}
+
+}
